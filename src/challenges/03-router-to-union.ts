af3e5ffb5ce67c6f809034a71e7ae14e8c625269
Yandall <jhonstevenkill22@gmail.com>
2023-03-04 15:57:25 -0500

    New challenges added

---
+++ src/challenges/03-router-to-union.ts
@@ -0,0 +1,27 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+type Router =
+  | {
+      url: "auth/login";
+      method: "POST";
+      handler: () => void;
+    }
+  | {
+      url: "aisle/:number/product/:id";
+      method: "GET";
+      handler: () => object;
+    }
+  | {
+      url: "profile/user/:id";
+      method: "GET";
+      handler: () => object;
+    };
+
+type Urls = never;
+
+type Test = Expect<
+  Equal<
+    Urls,
+    "/auth/login" | "/aisle/:number/product/:id" | "/profile/user/:id"
+  >
+>;
